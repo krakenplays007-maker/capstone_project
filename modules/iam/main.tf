cbe281f4f6fc4139433d06fe4d97d91383e54b62
Devansh Tiwari <krakenplays007@gmail.com>
2026-04-11 23:41:14 +0530

    fix: use WORKSPACE for copy, fix target syntax for count modules

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -9,6 +9,11 @@ resource "google_service_account" "vm_service_account" {
   account_id   = var.vm_service_account_id
   display_name = var.vm_service_account_display_name
   project      = var.gcp_project_id
+
+  lifecycle {
+    ignore_changes  = [display_name]
+    prevent_destroy = false
+  }
 }
 
 resource "google_project_iam_member" "vm_service_account_roles" {
